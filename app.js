0b1ec0090a783472e6afc438719e6e6f7f707768
golukr9876 <golukumar02052007@gmail.com>
2025-06-07 08:10:27 +0530

    change to local host

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
      cors: {
-        origin: "http://192.168.58.148:5173",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST"],
         credentials: true,
     },
@@ -19,7 +19,7 @@ const io = new Server(server, {
 
 app.use(
      cors({
-        origin: "http://192.168.58.148:5173",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST"],
         credentials: true,
     })
